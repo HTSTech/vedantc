9af273e719ba3b061098a92a1aa480666b8692e5
HTSpankaj <pankaj.agade@htstechsolutions.com>
2022-09-08 16:01:25 +0530

    feat: Added OUTPUT.md programmatically

---
+++ pages/Home/index.jsx
@@ -0,0 +1,51 @@
+
+  import { useEffect } from 'react';
+  import axios from 'axios';
+  import Link from 'next/link';
+  import Header from "../Header/index";
+  import Footer from "../Footer/index";
+  
+  export default function Home() {
+  
+    useEffect(() => {
+      getData();
+    }, [])
+   
+    async function getData() {
+      
+      axios.get("https://nqhkpgaezalnpxtizxzv.supabase.co/storage/v1/object/public/projects/c4a5b628-cde0-44ea-b1f7-85770f75d5ef/vedantc/Home/index.html").then(res=>{
+        console.log("res=> ",res);
+        document.getElementById("HomeID").innerHTML = res.data;
+        
+        //* css
+        axios.get("https://nqhkpgaezalnpxtizxzv.supabase.co/storage/v1/object/public/projects/c4a5b628-cde0-44ea-b1f7-85770f75d5ef/vedantc/Home/style.css").then(CssRes=>{
+          console.log("CssRes", CssRes);
+          let createCssElement = document.createElement("style");
+          // createCssElement.nodeValue = CssRes.data;
+          createCssElement.appendChild(document.createTextNode(CssRes.data));
+          
+          document.getElementById("HomeID").appendChild(createCssElement)
+          axios.get("https://nqhkpgaezalnpxtizxzv.supabase.co/storage/v1/object/public/projects/c4a5b628-cde0-44ea-b1f7-85770f75d5ef/vedantc/Home/script.js").then(JsRes=>{
+            console.log("JsRes", JsRes);
+            eval(JsRes.data)
+          })
+        })
+      })
+  
+    }
+    
+  
+    return (
+      <div>
+        <>
+          <Header />
+        </>
+        
+        <div id='HomeID'></div>
+
+        <>
+          <Footer />
+        </>
+      </div>
+    )
+  }
